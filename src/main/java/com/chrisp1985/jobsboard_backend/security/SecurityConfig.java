7546f014acbca3745f11a42957ba3e7696c7f784
unknown <chriseparsons@hotmail.co.uk>
2025-06-03 21:44:55 +0100

    Add CORS support for use with Spring Boot security.

--- src/main/java/com/chrisp1985/jobsboard_backend/security/SecurityConfig.java
+++ src/main/java/com/chrisp1985/jobsboard_backend/security/SecurityConfig.java
@@ -3,17 +3,11 @@ package com.chrisp1985.jobsboard_backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
@@ -21,7 +15,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/jobs/**").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/jobs/**").hasRole("admin")
                         .requestMatchers(HttpMethod.PUT, "/api/jobs/**").hasRole("admin")
